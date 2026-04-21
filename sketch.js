577c1e770e2a57f8a33b9db9d464f843c21a35ce
Tai110466 <tai0918019229@gmail.com>
2026-04-21 10:40:22 +0800

    2.1

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let capture;
+let overlay;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -8,6 +9,16 @@ function setup() {
 
 function draw() {
   background('#e7c6ff');
+
+  // 當攝影機準備好且 overlay 尚未建立時，建立一個與攝影機同寬高的畫布
+  if (!overlay && capture.width > 0) {
+    overlay = createGraphics(capture.width, capture.height);
+  }
+
+  if (overlay) {
+    overlay.clear(); // 清除上一幀內容，保持透明背景
+    // 你可以在這裡繪製任何想顯示在視訊上方的內容
+  }
 
   // 計算影像的大小（畫布寬高的 60%）
   let videoW = width * 0.6;
@@ -22,5 +33,10 @@ function draw() {
   translate(x + videoW, y); 
   scale(-1, 1);
   image(capture, 0, 0, videoW, videoH);
+  
+  // 將 overlay 顯示在視訊畫面的上方
+  if (overlay) {
+    image(overlay, 0, 0, videoW, videoH);
+  }
   pop();
 }
