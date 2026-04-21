b1ec811c5d7104c56c1c7cd14664bf171984d0cf
Tai110466 <tai0918019229@gmail.com>
2026-04-21 10:54:21 +0800

    2.2

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let capture;
 let overlay;
+let hearts = []; // 儲存愛心物件的陣列
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -17,7 +18,28 @@ function draw() {
 
   if (overlay) {
     overlay.clear(); // 清除上一幀內容，保持透明背景
-    // 你可以在這裡繪製任何想顯示在視訊上方的內容
+    
+    // 每隔幾幀產生一個新愛心
+    if (frameCount % 6 === 0) {
+      hearts.push({
+        x: random(overlay.width),
+        y: overlay.height + 20,
+        size: random(15, 30),
+        speed: random(1, 3),
+        opacity: 255
+      });
+    }
+
+    // 更新並繪製所有愛心
+    for (let i = hearts.length - 1; i >= 0; i--) {
+      let h = hearts[i];
+      h.y -= h.speed;     // 往上升
+      h.opacity -= 2;     // 逐漸透明
+      drawHeart(overlay, h.x, h.y, h.size, h.opacity);
+      
+      // 移除完全透明的愛心，優化效能
+      if (h.opacity <= 0) hearts.splice(i, 1);
+    }
   }
 
   // 計算影像的大小（畫布寬高的 60%）
@@ -39,4 +61,17 @@ function draw() {
     image(overlay, 0, 0, videoW, videoH);
   }
   pop();
+}
+
+// 繪製愛心的輔助函式
+function drawHeart(pg, x, y, size, opacity) {
+  pg.push();
+  pg.fill(255, 100, 100, opacity);
+  pg.noStroke();
+  pg.beginShape();
+  pg.vertex(x, y);
+  pg.bezierVertex(x - size, y - size, x - size * 1.5, y + size / 2, x, y + size);
+  pg.bezierVertex(x + size * 1.5, y + size / 2, x + size, y - size, x, y);
+  pg.endShape(CLOSE);
+  pg.pop();
 }
